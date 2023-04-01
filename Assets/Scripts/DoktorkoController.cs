ef96be4afe7cd6bde94f02cfb3bf2324fd2fd9e5
Rumko <matorumanovic@gmail.com>
2023-04-01 23:28:33 +0200

    Final

--- Assets/Scripts/DoktorkoController.cs
+++ Assets/Scripts/DoktorkoController.cs
@@ -98,7 +98,7 @@ public class DoktorkoController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "ground")
+        if(collision.gameObject.tag == "ground" || collision.gameObject.tag == "platforma")
         {
             isGrounded = true;
         }
